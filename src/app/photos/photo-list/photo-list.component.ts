cafd744d0d5eb39c607a8697738a5f78308424c5
João Prates <jovictor833@gmail.com>
2023-11-01 19:32:38 -0300

    create new search component and brightness directive

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Photo } from '../photo/photo';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
@@ -10,11 +8,10 @@ import { PhotoService } from '../photo/photo.service';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent  implements OnInit, OnDestroy {
+export class PhotoListComponent  implements OnInit{
 
   photos: Photo[] = [];
   filter: string = '';
-  debounce: Subject<string> = new Subject<string>();
   hasMore: boolean = false;
   currentPage: number = 1;
   userName: string = '';
@@ -27,27 +24,15 @@ export class PhotoListComponent  implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.userName = this.activatedRoute.snapshot.params['userName'];
     this.photos = this.activatedRoute.snapshot.data['photos']
-    this.debounce.pipe(debounceTime(300)).subscribe(filter => this.filter = filter)
-  }
-
-  ngOnDestroy(): void {
-    this.debounce.unsubscribe();
-  }
-
-  onKeyUp(event: Event): string {
-    if (event instanceof InputEvent) {
-      return (event.target as HTMLInputElement).value;
-    } 
-    return '';
   }
 
   load() {
     this.photoService
         .listFromUserPaginated(this.userName, ++this.currentPage)
         .subscribe(photos => {
-          this.photos = this.photos.concat(photos);
-          if(!photos.length) this.hasMore = false;
+            this.filter = '';
+            this.photos = this.photos.concat(photos);
+            if(!photos.length) this.hasMore = false;
         });
-}
-
+  }
 }
